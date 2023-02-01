89b42098c37948793b3757602feaf8e4d3c8287b
EuphieSS <samma118@hotmail.com>
2023-02-01 04:58:25 +0000

    Initial commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("textarea").keyup(function() {
+    const $charCounter = $(this).parent().children(".tweet-button-counter").children(".counter");
+    const limit = 140;
+    let $remainder = limit - $(this).val().length;
+    $charCounter.text($remainder);
+
+    if ($remainder < 0) {
+      $charCounter.css("color", "rgb(234, 11, 111)");
+    } else {
+      $charCounter.css("color", "#545149");
+    }
+  });
+});
