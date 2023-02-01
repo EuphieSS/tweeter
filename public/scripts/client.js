33414754f0345494a8d0273c6e06f461878e1743
EuphieSS <samma118@hotmail.com>
2023-02-01 23:29:34 +0000

    Implement dynamix tweet functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-const createTweetElement = (tweetObj) => {
-  const oldTweet = `
+
+const renderTweets = function(tweets) { //loop through a data object and feed each element to createTweetElement
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('.old-tweet').append($tweet);
+  }
+};
+
+
+const createTweetElement = (tweetObj) => { //turn a tweet object into a HTML tweet element
+  const $oldTweet = `
     <article>
       <header>
         <div>
@@ -27,27 +61,11 @@ const createTweetElement = (tweetObj) => {
     </article>
     `;
 
-  return oldTweet;
+  return $oldTweet;
 };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
 
-const $tweet = createTweetElement(tweetData);
 
 $(function() {
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.old-tweet').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 });
