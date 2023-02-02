a10321602c9c190f5f6a75076284ec9e0ba2cb90
EuphieSS <samma118@hotmail.com>
2023-02-02 04:30:49 +0000

    Add form data validation feature

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 
 const renderTweets = function(tweets) { //loop through a data object and feed each element to createTweetElement
   for (const tweet of tweets) {
@@ -73,13 +47,26 @@ const loadTweets = function() { //make a request to /tweets and receive the arra
 
 
 $(function() {
-  
   $(".tweet-form").submit(function(event) {
     event.preventDefault();
+
+    const tweetLength = $(this).find("#tweet-text").val().length;
+    //.find selector gets the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element
+
+    if (!tweetLength) {
+      alert("Please enter your tweet!");
+      return;
+    }
+
+    if (tweetLength > 140) {
+      alert("Oops your tweet is too long!");
+      return;
+    }
+      
     const $newTweet = $(this).serialize();
     $.post("/tweets/", $newTweet, function() { //send data to the server
       loadTweets(); //if the request succeeds, this function will execute
     });
+    
   });
-
 });
