ed91f3feda5c5bcb284459da6132733f83c80e9b
EuphieSS <samma118@hotmail.com>
2023-02-02 03:51:08 +0000

    Add timeago feature and a function to request data from /tweets/

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 
 const renderTweets = function(tweets) { //loop through a data object and feed each element to createTweetElement
@@ -39,7 +39,7 @@ const renderTweets = function(tweets) { //loop through a data object and feed ea
 };
 
 
-const createTweetElement = (tweetObj) => { //turn a tweet object into a HTML tweet element
+const createTweetElement = function(tweetObj) { //turn a tweet object into a HTML tweet element
   const $oldTweet = `
     <article>
       <header>
@@ -51,7 +51,7 @@ const createTweetElement = (tweetObj) => { //turn a tweet object into a HTML twe
       </header>
       <p>${tweetObj.content.text}</p>
       <footer>
-        <span>${tweetObj.created_at}</span>
+        <span>${timeago.format(tweetObj.created_at)}</span>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -65,15 +65,21 @@ const createTweetElement = (tweetObj) => { //turn a tweet object into a HTML twe
 
 };
 
+const loadTweets = function() { //make a request to /tweets and receive the array of tweets as JSON
+  $.getJSON("/tweets/", function(data) {
+    renderTweets(data);
+  });
+};
 
 
 $(function() {
-  renderTweets(data);
-
+  
   $(".tweet-form").submit(function(event) {
     event.preventDefault();
     const $newTweet = $(this).serialize();
-    $.post("/tweets/", $newTweet);
-  })
+    $.post("/tweets/", $newTweet, function() { //send data to the server
+      loadTweets(); //if the request succeeds, this function will execute
+    });
+  });
 
 });
